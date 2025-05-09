500bbf9cbfb064134d359695b4c6d4ffcc972519
praveenrai-123 <praveenrai.jsg@gmail.com>
2025-05-09 22:38:32 +0530

    tast done by dev1

--- gitproj01/src/main/java/com/prr/main/App.java
+++ gitproj01/src/main/java/com/prr/main/App.java
@@ -1,10 +1,14 @@
 package com.prr.main;
 
-/**
- * Hello world!
- */
+
 public class App {
+	
+	public int sum(int x, int y) {
+		return x+y;
+	}
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Main");
+        App p = new App();
+        System.out.println(p.sum(16,56));
     }
 }
--- gitproj01/src/test/java/com/prr/main/AppTest.java
+++ gitproj01/src/test/java/com/prr/main/AppTest.java
@@ -1,19 +1,53 @@
 package com.prr.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
+    
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositive() {
+    	App p = new App();
+    	int expected = 500;
+    	int actual=p.sum(300, 200);
+    	assertEquals(expected,actual);
+       
+    }
+    
+    @Test
+    public void testSumWithNegative() {
+    	App p = new App();
+    	int expected = -500;
+    	int actual=p.sum(-300, -200);
+    	assertEquals(expected,actual);
+       
+    }
+    @Test
+    public void testSumWithMixValue() {
+    	App p = new App();
+    	int expected = 100;
+    	int actual=p.sum(300, -200);
+    	assertEquals(expected,actual);
+       
+    }
+    @Test
+    public void testSumWithZero() {
+    	App p = new App();
+    	int expected = 0;
+    	int actual=p.sum(0, 0);
+    	assertEquals(expected,actual);
+       
+    }
+    @Test
+    public void testMix() {
+    	App p = new App();
+    	int expected = 36;
+    	int actual=p.sum(78, -42);
+    	assertEquals(expected,actual);
+       
     }
 }
